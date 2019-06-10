60ba2f88393ec979bb222e73998f7db7828fa321
Kirils Kravcovs <kkravcovs@gmail.com>
2019-06-10 17:55:39 +0300

    Sql

---
+++ PD_Sql.sql
@@ -0,0 +1,64 @@
+﻿CREATE TABLE Pasutijums(
+	Id INT,
+	Summa DECIMAL(8, 2),
+	BoughtOn DATE
+)
+INSERT INTO Pasutijums
+VALUES(15001, 150.5, '2018-10-05')
+INSERT INTO Pasutijums
+VALUES(15002, 270.65, '2018-09-10')
+INSERT INTO Pasutijums
+VALUES(16000, 65.26, '2018-10-05')
+INSERT INTO Pasutijums
+VALUES(16005, 110.5, '2018-08-17')
+INSERT INTO Pasutijums
+VALUES(17000, 948.5, '2018-09-10')
+
+SELECT BoughtOn FROM Pasutijums -- 2.
+
+SELECT Summa FROM Pasutijums --3.
+ORDER BY Summa DESC
+
+SELECT Id FROM Pasutijums	--4.
+WHERE Summa > 150
+
+SELECT * From Pasutijums	--5.
+WHERE BoughtOn like '2018-10%'
+
+INSERT INTO Pasutijums	--6.
+VALUES (18000, 666.66, '2019-06-10')
+
+UPDATE Pasutijums	--7.
+SET Summa = 1000
+WHERE Id = 17000
+
+DELETE FROM Pasutijums	--8.
+WHERE Id = 18000
+
+CREATE TABLE Klienti(	--9.
+	Id INT,
+	Vards NVARCHAR(50),
+	Uzvards NVARCHAR(50)
+)
+INSERT INTO Klienti
+VALUES(10000, 'John', 'Smith')
+INSERT INTO Klienti
+VALUES(11000, 'Kate', 'Miller')
+
+ALTER TABLE Pasutijums	--10.
+ADD ClientId INT
+
+SELECT * FROM Pasutijums
+
+INSERT INTO Pasutijums(ClientId)
+VALUES(10000);
+INSERT INTO Pasutijums(ClientId)
+VALUES(10000);
+INSERT INTO Pasutijums(ClientId)
+VALUES(11000);
+INSERT INTO Pasutijums(ClientId)
+VALUES(10000);
+INSERT INTO Pasutijums(ClientId)
+VALUES(11000);
+
+DELETE FROM Pasutijums WHERE Summa IS NULL
---
+++ SQLQuery1.sql
@@ -0,0 +1,42 @@
+﻿/*CREATE TABLE Users(
+	Id INT,
+	Username NVARCHAR(50),	--varchar doesnt assing sepific qty in memory
+	CreatedOn DATE,
+	Balance DECIMAL(8, 2) -- 8 cipari kopa, no kuriem divi aiz komata
+)*/
+
+SELECT * FROM Users -- Id, Username lietas ko atlasit, var ari lietot * ja atlasit visu
+
+INSERT INTO Users(Id, Username, CreatedOn, Balance)
+VALUES(1, 'Kirils', '2019-06-10', 799.99)
+
+UPDATE Users	--ieraksta atjaunosana
+SET Balance=1000, Username='KirKrav'
+WHERE Id = 1;	--atjauno tikai id ar 3
+
+DELETE FROM Users
+WHERE Id=1
+
+SELECT * FROM Users
+WHERE Username like '%iril%' -- var izmantot vienadibas zimes vieta "like" lai mekletu pec dalas, un "%" zimi lai mekletu dalu %iri%
+AND Balance >=500
+AND Balance <=1000
+
+SELECT * FROM Users
+WHERE CreatedOn > '2019-06-01'
+
+SELECT count (*) FROM Users
+WHERE id = 2;
+
+CREATE TABLE Orders(
+Id INT,
+UserID INT,
+ItemTitle NVARCHAR(100)
+)
+
+INSERT INTO Orders (Id, UserID, ItemTitle)
+VALUES (1, 2, 'Prece 1')
+
+SELECT o.* FROM Orders o, Users u	--atlasam no divam tabulam, ar prefiksi
+WHERE o.UserID = u.Id
+AND u.Username = 'Kirils'; 
