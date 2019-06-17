9e517aebb7034387791ab6e04403d7f0e33f9e85
Kirils Kravcovs <kkravcovs@gmail.com>
2019-06-17 18:02:18 +0300

    Forms and user lists

--- FirstWebApplication/Controllers/UserController.cs
+++ FirstWebApplication/Controllers/UserController.cs
@@ -20,5 +20,58 @@ namespace FirstWebApplication.Controllers
 
             return View(users);
         }
+        [HttpGet]//so meodi var izmantot tikai datu sanemsanai
+        public ActionResult Edit(int id)
+        {
+            User user = null;
+            using (var db = new DB())
+            {
+                user = db.Users.Find(id);
+            }
+
+            return View(user);
+        }//bloks prieks lietotaja bazes attelosanas/\
+        [HttpPost]//so metodi var izmantot tikai datu nosutisanai
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new DB()) //pieslegums pie datubazes
+                {
+                    User oldUser = db.Users.Find(user.Id);
+
+                    oldUser.FirstName = user.FirstName;
+                    oldUser.LastName = user.LastName;
+
+                    db.SaveChanges();
+                }
+            }
+            return View(user);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new DB())
+                {
+                    user = db.Users.Add(new Models.User()
+                    {
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                    });
+
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index", new { id = user.Id });
+            }
+            return View();
+        }
     }
 }
