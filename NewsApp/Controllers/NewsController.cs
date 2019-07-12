7b524f350caa76be391d024193d627440374d591
axelll030 <kkravcovs@gmail.com>
2019-07-12 22:46:40 +0300

    wer

--- NewsApp/Controllers/NewsController.cs
+++ NewsApp/Controllers/NewsController.cs
@@ -10,7 +10,7 @@ namespace NewsApp.Controllers
     public class NewsController : Controller
     {
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Topics()
         {
             List<Topic> topics = new List<Topic>();
             using (var db = new News_DB())
@@ -18,6 +18,33 @@ namespace NewsApp.Controllers
                 topics = db.Topics.ToList();
             }
             return View(topics);
+        }
+        [HttpGet]
+        public ActionResult Main()
+        {
+            List<News> news = new List<News>();
+            using (var db = new News_DB())
+            {
+                news = db.News.OrderByDescending(d => d.Date).ToList();
+            }
+            return View(news);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NewsText(int id)
+        {
+            using (var db = new News_DB())
+            {
+                News news = db.News.Find(id);
+                return View(news);
+            }
         }
     }
 }
